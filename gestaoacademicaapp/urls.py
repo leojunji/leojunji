0e775c976f92fc2b74cd3bb6fb3c4f5474db2aef
leojunji <leojunji@outlook.com>
2021-05-12 00:54:36 +0000

    Atualização do login do site no github

---
+++ gestaoacademicaapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+"""todas as urls referente a gestãoacademica deverão estar nesse aquivo"""
+urlpatterns = [
+    path('', views.listaMatriculas, name='lista-disciplinamatricula'),
+    path('fazerMatricula/<int:id>', views.fazerMatricula, name="alterarstatusmatricula"),
+]
--- projetohelloworld/settings.py
+++ projetohelloworld/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'helloworldapp',
     'gestaoacademicaapp',
+    'crispy_forms',
 ]
 
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'projetohelloworld.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/accounts/login'
